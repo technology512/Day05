019eac57a3023d163216d969b9994f050db162a1
technology512 <ald.wapper@gmail.com>
2021-03-13 00:48:38 +0100

    escape sequences in strings

---
+++ ex03/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+
+    myString = "FirstLinenewlinetabbackslashSecondLinenewlineThirdLine";
+    myString = "FirstLine\n\tSecondLine\nThirdLine";
+
+    return myString;
+}
+console.log(main());
+module.exports = main;
